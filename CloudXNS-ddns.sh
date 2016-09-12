7cfd6c7fbd4f773ba72c48425e373a5dfd0335d4
Kuretru <kuretru@gmail.com>
2016-09-13 00:12:34 +0800

    Added LICENSE

--- CloudXNS-ddns.sh
+++ CloudXNS-ddns.sh
@@ -2,31 +2,39 @@
 #==================================================
 # OS Required: Linux with curl
 # Description: CloudXNS DDNS on bash
-# Author: kuretru
-# Version: 1.0.160827
+# Author: Kuretru
+# Version: 1.1.160913
+# Github: https://github.com/kuretru/CloudXNS-DDNS/
 #==================================================
 
 #API Key
-i_api_key="12345678912345678912345678912345"
+api_key=""
  
 #Secret Key
-i_secret_key="1234567891234567"
+secret_key=""
  
-#含主机记录的完整域名(如www.cloudxns.net.)
-i_domain="www.cloudxns.net."
+#Domain name
+#e.g. domain="www.cloudxns.net."
+domain=""
 
-#需要DDNS的IP的网卡
-i_interface="eth0"
- 
-i_value=$(ifconfig $i_interface|grep 'inet addr'|awk -F ":" '{print $2}'|awk '{print $1}')
-i_url="https://www.cloudxns.net/api2/ddns"
-i_time=$(date -R)
-i_data="{\"domain\":\"${i_domain}\",\"ip\":\"${i_value}\",\"line_id\":\"1\"}"
-i_mac_raw="$i_api_key$i_url$i_data$i_time$i_secret_key"
-i_mac=$(echo -n $i_mac_raw | md5sum | awk '{print $1}')
-i_header1="API-KEY:"$i_api_key
-i_header2="API-REQUEST-DATE:"$i_time
-i_header3="API-HMAC:"$i_mac
-i_header4="API-FORMAT:json"
+#WAN Network Interface Card
+#e.g. interface=""     -> If it's empty, CloudXNS will automatic get your public IP
+#or   interface="ppp0" -> on tomato
+#or   interface="wan1" -> on OpenWRT
+interface=""
 
-curl -k -X POST -H $i_header1 -H "$i_header2" -H $i_header3 -H $i_header4 -d "$i_data" $i_url
+if  [ -n "$interface" ] ;then
+	value=$(ifconfig $interface|grep 'inet addr'|awk -F ":" '{print $2}'|awk '{print $1}')
+fi
+url="https://www.cloudxns.net/api2/ddns"
+time=$(date -R)
+data="{\"domain\":\"${domain}\",\"ip\":\"${value}\",\"line_id\":\"1\"}"
+mac_raw="$api_key$url$data$time$secret_key"
+mac=$(echo -n $mac_raw | md5sum | awk '{print $1}')
+header1="API-KEY:"$api_key
+header2="API-REQUEST-DATE:"$time
+header3="API-HMAC:"$mac
+header4="API-FORMAT:json"
+
+result=$(curl -k -X POST -H $header1 -H "$header2" -H $header3 -H $header4 -d "$data" $url)
+echo "${result} ${time} ${data}" >> $(pwd)/cloudxns-ddns.log
---
+++ README.md
@@ -0,0 +1,34 @@
+# CloudXNS DDNS on bash with curl
+看到很多CloudXNS的DDNS客户端都是基于官方的Python SDK做的，于是根据官方的API文档撸了个bash下的一个轮子。
+
+### 特点
+1. 系统需支持curl命令，适合在闪存容量小，不能安装Python的路由器上运行。
+2. 在CloudXNS[申请API Key](https://www.cloudxns.net/AccountManage/apimanage.html)，只需要在脚本中填写API Key，不需要提供账号密码，绿色安全。
+3. 不像DNSPod那样繁琐，需要先通过客户端查询域名ID、记录ID，只需提供需要DDNS的域名。
+
+### 用法
+1. 在CloudXNS获得API Key后，将API Key、Secret Key填入脚本。
+2. 在CloudXNS添加需要DDNS的域名，并在脚本填写该域名。  
+`domain="www.cloudxns.net."`
+3. 设置好具有公网IP的网卡
+ ```
+ interface=""      #留空时，CloudXNS则会自动获取你的公网IP
+ interface="ppp0"  #tomato路由器使用ppp0作为网卡名
+ interface="wan1"  #OpenWRT路由器使用wan1作为网卡名
+ ```  
+4. 脚本会在执行过后自动在当前目录下生成名为*cloudxns-ddns.log*的日志文件，方便日后查看，如果不需要此功能，将最后一行注释即可。  
+`#echo "${result} ${time} ${data}" >> $(pwd)/cloudxns-ddns.log`
+5. 执行`sh CloudXNS-ddns.sh`。
+
+### 路由器设置
+###### tomato
+在*当WAN联机时*事件中添加本脚本。  
+###### OpenWRT
+* 方法1：安装ddns-scripts和luci-app-ddns，在DDNS服务下添加本脚本。
+* 方法2：将99-ddns放至/etc/hotplug.d/iface下，网卡启动时将自动运行本脚本，注意修改脚本中网卡名。
+
+### LICENSE
+GNU General Public License v3.0
+
+### 参考资料 :paperclip:
+1. https://www.cloudxns.net/Support/detail/id/1361.html -- CloudXNS 官方 API 文档
